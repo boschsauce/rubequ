22d380f55f845a0d5cdfb0e100243363a36e8fc9
Willy <wwilimek@gmail.com>
2013-07-24 23:16:34 -0500

    Updating to move the secret token to a file in the root directory. execute rake secret > secret_token in the project root directory to get it working.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,15 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Rubequ::Application.config.secret_key_base = '79b88b799c4ce2f9667d5f4a20b05ae72f38f0be886bb9c56d6add5bc4cce8879bebbed0402603c6ef36f8ad7f0db37c06eacbeb0d47f4b6d933c341315ae820'
+#https://github.com/gedankenstuecke/snpr/blob/master/config/initializers/secret_token.rb
+#execute in the project root. 
+#rake secret > secret_token
+begin 
+  token_file = Rails.root.to_s + "/secret_token"
+  to_load = open(token_file).read
+  Rubequ::Application.configure do
+    config.secret_token = to_load
+  end
+rescue LoadError, Errno::ENOENT => e
+  raise "Secret token couldn't be loaded! Error: #{e}"
+end
