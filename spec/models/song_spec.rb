3486459477a2796a4444e35c958bb860b009607c
willyw <wwilimek@gmail.com>
2013-08-23 10:30:49 -0500

    Adding some tests back that I accidently deleted ooops.

--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -46,6 +46,56 @@ describe Song do
     @song.band_and_song_name_formatted.should eq "LMAFO I'm Sexy And I Know It"
   end
 
+  describe "all in queue" do
+    it "should return all songs in the queue" do
+      RubequMpd::Mpd.any_instance.stub(:queued_song_ids).and_return(["4", "2", "1"])
+      RubequMpd::Mpd.any_instance.stub(:current_song_id).and_return("2")
+      Song.stub(:unscoped).and_return(Song)
+      Song.should_receive(:where).with({:id => ["4", "1"]})
+      Song.all_in_queue
+    end
+    it "should delete current song from queue songs" do
+      queued_song_ids = ["1", "2", "3"]
+      RubequMpd::Mpd.any_instance.stub(:queued_song_ids).and_return(queued_song_ids)
+      RubequMpd::Mpd.any_instance.stub(:current_song_id).and_return("2")
+      queued_song_ids.should_receive(:delete).with("2")
+      Song.all_in_queue
+    end
+
+    it "should sort the songs in the queue based on the ids that come back from mpd queue song ids" do
+      queued_song_ids = ["1", "2", "3"]
+      RubequMpd::Mpd.any_instance.stub(:queued_song_ids).and_return(queued_song_ids)
+      RubequMpd::Mpd.any_instance.stub(:current_song_id).and_return(nil)
+
+      @song_one   = FactoryGirl.build(:song, :id => "1")
+      @song_two   = FactoryGirl.build(:song, :id => "2")
+      @song_three = FactoryGirl.build(:song, :id => "3")
+
+      unsorted_songs_array = [@song_three, @song_one, @song_two]
+
+      Song.stub_chain(:unscoped, :where).with({:id => queued_song_ids}).and_return(unsorted_songs_array)
+      Song.all_in_queue.should eq [@song_one, @song_two, @song_three]
+    end
+  end
+
+  describe "current song " do
+    it "should return the nil when there is no current song" do
+      @mpd_mock.stub(:current_song).and_return(nil)
+      Song.current_song.should eq nil
+    end
+
+    it "should return the current song when current song is not blank and found in songs" do
+      @mpd_mock.stub(:current_song).and_return(Music.new("1.mp3"))
+      Song.stub(:by_file).and_return(@song)
+      Song.current_song.should eq @song
+    end
+
+    it "should validate current_song was called" do
+      @mpd_mock.should_receive(:current_song)
+      Song.current_song
+    end
+  end
+
   describe "music player controls " do
     it "should validate that next is called" do
       @mpd_mock.stub(:next)
